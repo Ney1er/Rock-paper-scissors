56a974c0aaa3dc2d5950ea3dfc17d63dbfa2ef02
Ney1er <pilardarryl@gmail.com>
2023-05-04 20:30:00 +0200

    add js code

--- script.js
+++ script.js
@@ -1,71 +1,98 @@
-const CHOICE = ["Rock", "Paper", "Scissor"]
+// Get the img moves from html
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+// Add click event listeners to the img moves
+rock.addEventListener("click", () => playRound("rock"));
+paper.addEventListener("click", () => playRound("paper"));
+scissors.addEventListener("click", () => playRound("scissors"));
 
 
+// Get a move for the computer
 function getComputerChoice() {
-  let randomIndex = Math.floor(Math.random() * CHOICE.length);
-  return CHOICE[randomIndex];
+    const moves = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * moves.length);
+    return moves[randomIndex];
+
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
+// Play one round of the game
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    const result = outcome(playerSelection, computerSelection);
 
-  const WIN = `You win! ${playerSelection.charAt(0).toUpperCase() +
-    playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() +
-    computerSelection.slice(1)}`;
-  const DRAW = "It's a draw!";
-  const LOSE = `You lose! ${computerSelection.charAt(0).toUpperCase() +
-    computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() +
-    playerSelection.slice(1)}`;
+    updatePlayerMove(playerSelection, computerSelection);
 
-  if ((playerSelection == "rock" && computerSelection == "scissor") ||
-    (playerSelection == "scissor" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "rock")) {
-    return WIN;
-  } else if (playerSelection === computerSelection) {
-    return DRAW;
-  } else {
-    return LOSE;
-  }
-}
-
-
-function game() {
-  let playerScore = 0
-  let computerScore = 0
-  let gaming = true
-
-  while (gaming) {
-    let playerSelection = prompt();
-    let computerSelection = getComputerChoice();
-
-    const WIN = `You win! ${playerSelection.charAt(0).toUpperCase() +
-      playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + 
-      computerSelection.slice(1)}`;
-    const DRAW = "It's a draw!";
-    const LOSE = `You lose! ${computerSelection.charAt(0).toUpperCase() + 
-      computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + 
-      playerSelection.slice(1)}`;
-    
-    let result = playRound(playerSelection, computerSelection);
-    if (result == WIN) {
-      playerScore++;
-      console.log(WIN);
-    } else if (result == LOSE) {
-      computerScore++;
-      console.log(LOSE);
+    // Update stats based on the outcome
+    if (result == "win") {
+        updateStats("win");
+    } else if (result == "lose") {
+        updateStats("lose");
     } else {
-      console.log(DRAW);
+        updateStats("draw");
+    }
+}
+
+
+// return the outcome of a round
+function outcome(playerSelection, computerSelection) {
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")) {
+        return "win";
+    } else if (playerSelection === computerSelection) {
+        return "draw";
+    } else {
+        return "lose";
+    }
+}
+
+// update html statistics
+function updateStats(result) {
+    const wins = document.querySelector("#win span");
+    const losses = document.querySelector("#lose span");
+    const draws = document.querySelector("#draw span");
+    const rounds = document.querySelector("#rounds span");
+
+    if (result == "win") {
+        wins.textContent = parseInt(wins.textContent) + 1;
+    } else if (result == "lose") {
+        losses.textContent = parseInt(losses.textContent) + 1;
+    } else if (result == "draw") {
+        draws.textContent = parseInt(draws.textContent) + 1;
     }
 
-    if (playerScore === 5) {
-      console.log(`You won! Player: ${playerScore}  Computer: ${computerScore}`)
-      gaming = false;
-    } else if (computerScore === 5) {
-      console.log(`You lose! Player: ${playerScore}  Computer: ${computerScore}`)
-      gaming = false;
-    }
-  }
+    rounds.textContent = parseInt(rounds.textContent) + 1;
 }
+
+// show player move
+function updatePlayerMove(playerSelection, computerSelection) {
+    const playerMoveImg = document.createElement("img");
+    const computerMoveImg = document.createElement("img");
+    const playerMove = document.getElementById("player-choice");
+    const computerMove = document.getElementById("computer-choice");
+  
+    // Check if images already exist and remove them if so
+    const existingPlayerMoveImg = document.getElementById("player-move");
+    if (existingPlayerMoveImg) {
+      existingPlayerMoveImg.remove();
+    }
+  
+    const existingComputerMoveImg = document.getElementById("computer-move");
+    if (existingComputerMoveImg) {
+      existingComputerMoveImg.remove();
+    }
+  
+    playerMoveImg.src = `Images/${playerSelection}.png`;
+    playerMoveImg.id = "player-move";
+  
+    computerMoveImg.src = `Images/${computerSelection}.png`;
+    computerMoveImg.id = "computer-move";
+  
+    playerMove.appendChild(playerMoveImg);
+    computerMove.appendChild(computerMoveImg);
+}
+
 
 
